e9a5fc73f0288ed8255d7e565a68648f44d9e2fe
preethiramiah <preethiramiah@gmail.com>
2025-02-11 00:47:43 +0530

    feat: calculate storage and display svgs

---
+++ README.md
@@ -0,0 +1,7 @@
+# Water Tank Problem
+
+## Deployed URL: https://water-tank-storage.vercel.app/
+
+Given `n` is always greater than -1, representing the block height. Now compute the units of water stored in-between the blocks. Build a Web Application (Frontend Solution) using Vanilla JavaScript and HTML/CSS to represent the solution. Generate SVG Shape like the ones shown below:
+
+![Input and Output svgs](/images/WaterTank.png)
--- scripts.js
+++ scripts.js
@@ -3,38 +3,103 @@ const calculateCapacity = (blockHeights) => {
 
   let totalCapacity = 0;
   let leftBlockHeight = 0;
-  let blockHeight = 0;
-  let emptyBlocks = 0;
+  let trappedWaterHeight = 0;
+  let trappedEmptyBlocks = 0;
+  const waterHeights = []; // To render output svg
+
   for (let i = 0; i < blockHeights.length; i++) {
     if (blockHeights[i] > 0) {
+      waterHeights.push(0);
       if (leftBlockHeight > 0) {
-        blockHeight = Math.min(leftBlockHeight, blockHeights[i]);
+        trappedWaterHeight = Math.min(leftBlockHeight, blockHeights[i]);
         leftBlockHeight = blockHeights[i];
       } else {
         leftBlockHeight = blockHeights[i];
       }
-    }
-    else {
+    } else {
       if (leftBlockHeight > 0) {
-        emptyBlocks++;
-      }
+        trappedEmptyBlocks++;
+        const rightBlock = blockHeights.slice(i).find(h => h > 0);
+        if (rightBlock > 0) waterHeights.push(Math.min(leftBlockHeight, rightBlock));
+        else waterHeights.push(0);
+      } else {
+        waterHeights.push(0);
+      }      
     }
 
-    if (blockHeight > 0) {
-      totalCapacity += blockHeight * emptyBlocks;
-      emptyBlocks = 0;
-      blockHeight = 0;
+    if (trappedWaterHeight > 0) {
+      totalCapacity += trappedWaterHeight * trappedEmptyBlocks;
+      trappedEmptyBlocks = 0;
+      trappedWaterHeight = 0;
     }
   }
-  document.querySelector('#outputValue').innerHTML = `${totalCapacity} units`;
+
+  return { totalCapacity, waterHeights };
+}
+
+const YELLOW = "#f2eb6a";
+const BLUE = "#19afe5";
+
+const createSvg = (blockHeights, waterHeights, isOutput) => {
+  const svgNS = "http://www.w3.org/2000/svg";
+  const svg = document.createElementNS(svgNS, "svg");
+  const rectWidth = 60;
+  const rectHeight = 25;
+  const cols = blockHeights.length;
+  const maxValue = Math.max(...blockHeights);
+  const rows = Math.min(10, maxValue + 1);
+  svg.setAttribute("width", rectWidth * cols);
+  svg.setAttribute("height", rectHeight * rows);
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      const rect = document.createElementNS(svgNS, "rect");
+      rect.setAttribute("x", col * rectWidth);
+      rect.setAttribute("y", row * rectHeight);
+      rect.setAttribute("width", rectWidth);
+      rect.setAttribute("height", rectHeight);
+      rect.setAttribute("stroke", "#000");
+      rect.setAttribute("stroke-width", "1");
+
+      let fillColor = "#fff";
+      const height = isOutput ? waterHeights[col] : blockHeights[col] || waterHeights[col];
+      const blockHeight = maxValue >= 10 ? (10 / maxValue) * height : height;
+      if (blockHeight > 0 && rows - blockHeight <= row) {
+        fillColor = isOutput ? BLUE : waterHeights[col] ? BLUE : YELLOW;
+      }
+
+      rect.setAttribute("fill", fillColor);
+      svg.appendChild(rect);
+    }
+  }
+
+  document.querySelector(isOutput ? "#outputDiv" : "#inputDiv").appendChild(svg);
+}
+
+const clearSvgs = () => {
+  const svgs = document.querySelectorAll('svg');
+  svgs.forEach(svg => svg.remove());
 }
 
 const showResults = () => {
+  clearSvgs();
+  document.querySelector('#result').style.visibility = 'hidden';
+
   const input = document.querySelector('#blocks')?.value;
   if (!input) return;
 
   document.querySelector('#result').style.visibility = 'visible';
   document.querySelector('#inputValue').innerHTML = `[${input}]`;
+
   const blockHeights = input.split(',').map(Number);
-  calculateCapacity(blockHeights);
+  const { totalCapacity, waterHeights } = calculateCapacity(blockHeights);
+  createSvg(blockHeights, waterHeights, true);
+
+  createSvg(blockHeights, waterHeights);
+  document.querySelector('#outputValue').innerHTML = `${totalCapacity} units`;
+  document.querySelector('footer').style.position = 'relative';
 }
+
+// 0,4,0,0,0,6,0,6,4,0
+
+// 0,40,0,0,0,60,0,60,40,0
